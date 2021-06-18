7d40bd8eb887421d91b67d5043d808a5b124901a
Gregory Saive <greg@evias.be>
2021-06-18 13:33:17 +0200

    (Bundle) Added Vue shims and TS mixins

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,12 @@
+/**
+ * This file is part of YourDLT Wallet Components shared under LGPL-3.0
+ * Copyright (C) 2021 Using Blockchain Ltd, Reg No.: 12658136, United Kingdom
+ *
+ * @package     YourDLT Wallet Components
+ * @author      Grégory Saive for Using Blockchain Ltd <greg@ubc.digital>
+ * @license     LGPL-3.0
+ */
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
